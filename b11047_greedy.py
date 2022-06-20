075ab38ef2a330c60eb1d9cfef9eceb9716898e4
DODO <thsehgml37@korea.ac.kr>
2022-06-20 22:49:31 +0900

    title

--- b11047_greedy.py
+++ b11047_greedy.py
@@ -1,11 +1,11 @@
-import sys
+import sys #동전 0
 reader = sys.stdin.readline
 n , k = map(int, reader().split())
 ans = 0
 
 arr = list(int( reader().rstrip() ) for _ in range(n)) 
 for i in range(n):
-        ans += ( k //arr[n-i-1] )
+        ans += ( k // arr[n-i-1] )
         k %= arr[n-i-1]
     
 print(ans)
