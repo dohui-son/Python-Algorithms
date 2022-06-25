315fa683704d9d52023739e20c88fe65eab06222
dohui-son <thsehgml37@korea.ac.kr>
2022-06-25 15:21:21 +0900

    Fix: stack with python dequeue

--- b3986_stack기본.py
+++ b3986_stack기본.py
@@ -1,14 +1,15 @@
-import sys #좋은 단어   #스택
+import sys #좋은 단어   #스택-----파이썬은 DEQUE 이용할것 st = collections.deque([])
+import collections
 reader = sys.stdin.readline
 n = int(reader().rstrip())
 ans = 0
 s = list(reader().rstrip() for _ in range(n) )
 for i in range(n):
     cnt = 0
-    st = []
+    st = collections.deque([])
     for c in s[i]:
-        if len(st) == 0 : st.append(c)    #스택 PUSH arr.append(값)   
-        elif st[-1] == c : print(st.pop())       #리스트 마지막 값 조회 arr[-1]   
+        if not st : st.append(c)    #스택 PUSH arr.append(값)   
+        elif st[-1] == c : st.pop()       #리스트 마지막 값 조회 arr[-1]   
         else : st.append(c)               #스택 POP arr.pop()
     if len(st) == 0 : ans += 1
 print(ans)
