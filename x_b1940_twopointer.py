6bd7602ea735c54c72c7b2a03010afd4a40d611d
dohui-son <thsehgml37@korea.ac.kr>
2022-06-25 13:54:14 +0900

    towpointer

--- x_b1940_twopointer.py
+++ x_b1940_twopointer.py
@@ -1,24 +1,19 @@
-#주몽  - 시간초과 투포인터 다시 풀어야함
-#투포인터(시간 더 짧음)와 COMBINATION을 서로 착각할 수 있다
+#주몽  - 시간초과 투포인터  #투포인터(SORT 필수!!!!!시간 더 짧음)와 COMBINATION을 서로 착각할 수 있다
+import sys
+reader = sys.stdin.readline
+n = int( reader().rstrip() ); m = int( reader().rstrip() )
+arr = list( map(int, reader().split())); arr.sort() ######## SORT 필수
+if m > 200000 : print(0); exit() 
 
-#include<bits/stdc++.h> 
-using namespace std;     
-int n, m, a[150001], cnt;
-int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cin >> n >> m; 
-	for(int i = 0; i < n; i++) cin >> a[i];
-	sort(a, a + n); 
-	if(m > 200000) cout << 0 << "\n";
-	else{
-		int lo = 0, hi = n - 1; 
-		while(lo < hi){
-			if(a[lo] + a[hi] == m)cnt++, lo++;
-			else if(a[lo] + a[hi] < m)lo++;
-			else hi--;
-		} 
-		cout << cnt << "\n";
-	} 
-}
+lo , hi, cnt  = 0, n-1, 0
+while lo < hi:
+	if arr[lo] + arr[hi] == m : 
+		cnt += 1 
+		lo += 1
+	elif arr[lo] + arr[hi] < m : lo += 1
+	else : hi -= 1
+	
+print(cnt)
+
+
 
