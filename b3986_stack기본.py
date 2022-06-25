6fee73e1907733dce966aab290465bea4a653207
dohui-son <thsehgml37@korea.ac.kr>
2022-06-25 15:18:09 +0900

    stack

--- b3986_stack기본.py
+++ b3986_stack기본.py
@@ -8,7 +8,7 @@ for i in range(n):
     st = []
     for c in s[i]:
         if len(st) == 0 : st.append(c)    #스택 PUSH arr.append(값)   
-        elif st[-1] == c : st.pop()       #리스트 마지막 값 조회 arr[-1]   
+        elif st[-1] == c : print(st.pop())       #리스트 마지막 값 조회 arr[-1]   
         else : st.append(c)               #스택 POP arr.pop()
     if len(st) == 0 : ans += 1
 print(ans)
