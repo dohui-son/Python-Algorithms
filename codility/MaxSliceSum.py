dc95094101e149659d3deadab98ef28a43e2d038
dohui-son <thsehgml37@korea.ac.kr>
2022-08-12 08:33:43 +0900

    prefix sum

--- codility/MaxSliceSum.py
+++ codility/MaxSliceSum.py
@@ -1,31 +1,23 @@
-# 틀림 수정 필요
-
-
-#  non-empty array A
-# 0 ≤ P ≤ Q < N
+#  O(N)
 from collections import defaultdict
 def solution(A):
-    n = len(A) #항상 0보다 큼
+    n = len(A) ;  maxi = max(A); mini = min(A); total = sum(A)
     if n == 1: return A[0]
-    maxi = max(A); mini = min(A)
     if maxi<=0: return maxi
-    total = sum(A)
+    if mini>=0: return total
     ans = maxi if maxi>total else total
-    if mini>=0: return ans
     psum = [0]*(n+1)
     dic = defaultdict(int)
-    dic[A[0]] = 0; smallest = [0]*(n+1); smallest[0] = A[0] 
+    dic[A[0]] = 0; smallest = [0]*(n+1) # smallest[i] 0번째부터 i번째 psum중에 가장 작은 값을 저장 
     for i in range(1,n+1):
         psum[i] = psum[i-1]+A[i-1]
         smallest[i] = min(smallest[i-1], psum[i])
         dic[psum[i]] = i
+    ans = max(max(psum),ans)
     B = psum.copy()
     B.sort(reverse=True)
     ans = max(ans, max(psum))
     for b in B:
+        if dic[b] == 0 : continue
         if b - smallest[dic[b]-1]  > ans and dic[b]> dic[b]-1 : ans = b - smallest[dic[b]-1]
-    
-
-
-
     return ans
