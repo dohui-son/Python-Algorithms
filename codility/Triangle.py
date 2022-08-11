a3780a18a7b193af9194396127668ee7e00e7470
dohui-son <thsehgml37@korea.ac.kr>
2022-08-11 11:32:21 +0900

    math

---
+++ codility/Triangle.py
@@ -0,0 +1,14 @@
+def solution(A):
+    n = len(A)
+    if n<=2: return 0
+    A.sort()
+    if A[n-1] <= 0: return 0
+    for i in range(0,n-2):
+        if A[i]>0:
+            summ = A[i]+A[i+1]
+            third = 0
+            if A[i] == A[i+1] == A[i+2] and A[i]>0:return 1 
+            for j in range(i+2,n):
+                if A[j]<A[i]+A[i+1] :return 1
+                else: break
+    return 0
