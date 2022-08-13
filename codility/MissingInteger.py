d9cb00e9299f72f63494c6cdf931d71f56b2ebf3
JHdesktop <thsehgml37@korea.ac.kr>
2022-08-13 23:19:49 +0900

    counting

--- codility/MissingInteger.py
+++ codility/MissingInteger.py
@@ -1,17 +1,12 @@
+# O(N) or O(N * log(N))
 def solution(A):
-    alen = len(A)
-    if alen == 0: return 1
-    maxi = 0; visit = 1
-    A.sort()
+    maxi = max(A); n = len(A)
+    if maxi <= 0: return 1
+    A.sort(); visit = 1
+    ans = 0
     for a in A:
-        if a<=0: maxi = a
-        else: 
+        if a >= 1 and not visit&(1<< a) :
             visit |= (1<<a)
-            if a>maxi: maxi = a
-    if maxi<=0: return 1
-    else:
-        full = (1<<maxi+1)-1
-        if visit==full: return maxi+1
-        bit = ~visit & full
-        smallest = bit&-bit
-        return bin(smallest)[::-1].index('1')
+            if a != ans+1: return ans+1
+            ans = a
+    return maxi+1
--- softeer/전광판.py
+++ softeer/전광판.py
@@ -1,78 +1,44 @@
-import sys
+import sys 
+sys.setrecursionlimit(5000)
+from collections import deque, defaultdict
 reader = sys.stdin.readline
-#도, 개, 걸, 윷, 모 로 각각 1, 2, 3, 4, 5칸
-mal = []
-score = [0,5,10,15,20,50,30,35,40,45,100,55,60,65,70,75,80,85,90,95,500,1000,275,250,150,125,175,150,300,350,400]
+num = defaultdict(int)
+num[0] = (  (1<<0) | (1<<1) | (1<<2) | (1<<3) | (1<<4)  | (1<<5) ) 
+num[1] = ( (1<<1) | (1<<2) )
+num[2] = ( (1<<0) | (1<<1) | (1<<6) | (1<<3) | (1<<4) )
+num[3] = ( (1<<0) | (1<<1) | (1<<6) | (1<<3) | (1<<2) )
+num[4] = ( (1<<1) | (1<<2) | (1<<5) | (1<<6) )
+num[5] = ( (1<<0) | (1<<2) | (1<<3) | (1<<5) | (1<<6) )
+num[6] = ( (1<<0) | (1<<2) | (1<<3) | (1<<4) | (1<<5) | (1<<6) )
+num[7] = ( (1<<1) | (1<<2) | (1<<5) | (1<<0) )
+num[8] = (1<<7)-1 
+num[9] = ( (1<<7)-1 ) ^ (1<<4)
+
 n = int(reader().rstrip())
-ans = [0]*n
-inp = []
-
-def nextIdx( now, kan ):
-  if now == 5: 
-    if kan<=2: return 22+(kan-1)
-    else : return 28 - (kan-3)
-  elif now == 10:
-    if kan<3: return 24 + (kan-1)
-    else : return 28+(kan-3)
-      
-  if now==24 or now == 25:
-    if now == 25: now = 24; kan +=1
-    if kan == 1:return 25
-    elif kan >= 5: return 20+(kan-5)
-    else: return 28+(kan-2)
-
-  if now == 27 or now == 26:
-    if now == 26: now-=1; kan+=1
-    if kan == 0: return now
-    if kan==1: return 26
-    else: return 15+(kan-2)
-    
-  if 28<= now <= 30  : #
-    if now == 29: now -=1; kan+=1
-    elif now == 30: now -=2; kan+=2
-    if kan<=2: return now+kan
-    if kan >= 4: return 21
-    elif kan == 3: return 20
-    
-  if now == 22 or now == 23: #
-    if now == 23: now = 22; kan+=1
-    if kan==1: return 23
-    if kan >= 5: return 15+(kan-5)
-    else: return 28 - (kan-2)
-  
-  if now == 28:
-    if kan<=2: return now+kan
-    else: 
-      if kan == 5 or kan ==4: return 21
-      elif kan == 3: return 20
-  if now < 21 : return now+kan if now+kan <= 21 else 21
-  
-
-
-def yoot(cur, ans_idx):
-  global ans
-  if cur == 10 or mal.count(21)==4 :
-    summ = 0
-    for i in range(4): summ += score[mal[i]]
-    ans[ans_idx] = max(ans[ans_idx], summ )
-  elif cur < 10:
-    kan = inp[ans_idx][cur]
-    for i in range(4):
-      if mal[i] == 21:continue
-      tmp = mal[i]
-      res = nextIdx(tmp, kan)
-      print(score[tmp], kan, score[res])
-      if res in mal:continue
-      mal[i] = res
-      yoot(cur+1, ans_idx)
-      mal[i] = tmp
-      
+ans = [7*5]*n
 for i in range(n):
-  l = list(map(int,reader().split()))
-  inp.append(l)
-  mal = [0]*4
-  yoot(0, i)
-  
-for i,val in enumerate(ans):
-  st = "#"+str(i)
-  print(st, ans[i])
+    a, b = reader().split()
+    arr = [0]*5; idx = 4 ; brr = [0]*5; ans = 0 ; bit = 0
+    for i in range( len(a)-1,-1,-1 ): arr[idx] = int(i); idx -= 1
+    idx = 4
+    for i in range( len(b)-1,-1,-1 ):  arr[idx] = int(i); idx -= 1
+    ansarr = [0]*5
+    for i in range(4): 
+        if brr[i]: # 1-9
+
+        else: # 0
+            if i == 0: # 전광판에 아무것도 안 띄워야하는 경우
+                ansarr[i] = -1
+                ans += bin(num[a[i]]).count('1')
+            elif ansarr[i-1] == -1: # 전광판에 아무것도 안 띄워야하는 경우
+                ansarr[i] = -1
+                ans += bin(num[a[i]]).count('1')
+            else:
+                
+
+
+
+
+
+        
+    
