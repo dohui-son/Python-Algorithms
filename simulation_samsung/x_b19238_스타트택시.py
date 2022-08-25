55def9b16f16c99b89c2e64c4b94083b53c4a7a7
DODO <thsehgml37@korea.ac.kr>
2022-08-25 16:50:42 +0900

    simulation

--- simulation_samsung/x_b19238_스타트택시.py
+++ simulation_samsung/x_b19238_스타트택시.py
@@ -1,73 +1,49 @@
-from collections import defaultdict,deque
-global n, m , fuel,g,sonn, ty,tx,sdx
+from collections import deque, defaultdict
+import heapq as hq
 
-n, m, fuel = map(int, input().split())
-g = [ [*map(int, input().split()) ] for _ in range(n)]
-ty, tx = map(int, input().split()); ty,tx = ty-1,tx-1
-sonn = deque()
-dir = [(0,1),(1,0),(-1,0),(0,-1)]
-for i in range(m):
-    a,b, aa, bb = map(int, input().split())
-    a,b,aa,bb = a-1,b-1,aa-1,bb-1
-    g[a][b] = i+2
-    sonn.append((a,b,aa,bb))
-ans, sdx = 0,0
+global n,m,fuel, ty,tx, dy,dx
 
-
-def sbfs():
-    global n, m, fuel, g, sonn, ty, tx, sdx
-    visit = [[0]*n for _ in range(n)]
-    visit[ty][tx] = 1; q = deque(); q.append((ty,tx))
-    nq = [];
-    sdx = -1
-
-    while q:
-        y,x = q.popleft()
-        for d in dir:
-            ny,nx = y+d[0], x+d[1]
-            if ny<0 or nx<0 or ny>=n or nx>=n: continue
-            if g[ny][nx] == 1 or visit[ny][nx]: continue
-
-            if g[ny][nx]>1 and  visit[y][x]<fuel : nq.append([visit[y][x], ny, nx ]) #시간초과나면 여기 수정해주
-            visit[ny][nx] = visit[y][x]+1
-            #if visit[ny][nx] > fuel+2: sdx = -1; return -1;
-            q.append((ny,nx))
-    if nq:
-        nq.sort()
-        sdx = g[nq[0][1] ][nq[0][2]]-2
-        return nq[0][0]
-    else : return int(2e9)
-
-def dbfs(dy, dx):
-    global n, m, fuel, g, sonn, ty, tx, sdx
-    visit = [[0]*n for _ in range(n)]
-    visit[ty][tx] = 1; q = deque(); q.append((ty,tx))
-
-    while q:
-        y,x = q.popleft()
-        for d in dir:
-            ny,nx = y+d[0], x+d[1]
-            if ny<0 or nx<0 or ny>=n or nx>=n: continue
-            if g[ny][nx] == 1 or visit[ny][nx]: continue
-
-            if ny==dy and nx == dx:
-                if visit[y][x]<=fuel : return visit[y][x]
-                else: return int(2e9)
-            visit[ny][nx] = visit[y][x]+1
-            q.append((ny,nx))
-    return int(2e9)
-
-
+n,m,fuel = map(int,input().split())
+g = [ [*map(int, input().split())] for _ in range(n)]
+ty,tx = map(int, input().split()) ; ty,tx = ty-1, tx-1
+start = deque()
+end = defaultdict(list)
 for _ in range(m):
-    fuel -= sbfs() #손님 못태워다주면 -1을
-    if fuel <= 0:print(-1);exit(0) #손님 태워다주기 실패
-    else: ty,tx = sonn[sdx][0], sonn[sdx][1]
-    usedfuel = dbfs(sonn[sdx][2],sonn[sdx][3])
-    if fuel-usedfuel < 0: print(-1);exit(0)  # 손님 태워다주기 실패
-    else:
-        fuel += usedfuel
-        g[ sonn[sdx][0] ][ sonn[sdx][1] ] = 0
-        ty, tx = sonn[sdx][2], sonn[sdx][3]
-        ans+=1
+    a,b,aa,bb = map(int, input().split())
+    start.append((a-1,b-1)); end[(a-1,b-1)] = (aa-1,bb-1)
 
-print(fuel) if ans == m else -1
+dir = [ (0,1),(1,0),(-1,0), (0,-1)]
+def bfs( arr ) :
+    global n, m, fuel, ty, tx
+
+    q = []; hq.heappush(q,(0,ty,tx))
+    visit = [[False]*n for _ in range(n)]
+    visit[ty][tx] = True
+    while q:
+        cost, y, x = hq.heappop(q)
+        if (y,x) in arr : return [cost, y, x]
+        for d in dir:
+            ny, nx = y+d[0], x+d[1]
+            if ny<0 or nx<0 or ny>=n or nx>=n: continue
+            if g[ny][nx]>0 or visit[ny][nx]==True: continue
+            visit[ny][nx] = True
+            hq.heappush(q,(cost+1,ny,nx))
+    return int(2e9), 0,0
+
+for i in range(m):
+    dy,dx = 0,0
+    used, yy, xx = bfs(start)
+    if fuel-used <= 0 : print(-1); exit(0)
+    else:
+        fuel -= used
+        ty, tx = yy, xx
+        dy,dx = end[(yy, xx )]
+
+    used, _, _ = bfs( [(dy,dx)] )
+
+    if fuel-used>=0 :
+        fuel += used
+        start.remove( (yy,xx) )
+        ty,tx = dy,dx
+    else: print(-1); exit(0)
+print(fuel)
