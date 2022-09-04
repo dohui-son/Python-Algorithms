4fa203513a2c47ab62d4dc069c6827a13f708223
DODO <thsehgml37@korea.ac.kr>
2022-09-04 21:48:21 +0900

    simulation

--- softeer/전광판.py
+++ softeer/전광판.py
@@ -1,3 +1,4 @@
+# 맞았음
 import sys 
 sys.setrecursionlimit(5000)
 from collections import deque, defaultdict
@@ -14,31 +15,17 @@ num[7] = ( (1<<1) | (1<<2) | (1<<5) | (1<<0) )
 num[8] = (1<<7)-1 
 num[9] = ( (1<<7)-1 ) ^ (1<<4)
 
+
 n = int(reader().rstrip())
 ans = [7*5]*n
 for i in range(n):
     a, b = reader().split()
-    arr = [0]*5; idx = 4 ; brr = [0]*5; ans = 0 ; bit = 0
-    for i in range( len(a)-1,-1,-1 ): arr[idx] = int(i); idx -= 1
-    idx = 4
-    for i in range( len(b)-1,-1,-1 ):  arr[idx] = int(i); idx -= 1
-    ansarr = [0]*5
-    for i in range(4): 
-        if brr[i]: # 1-9
-
-        else: # 0
-            if i == 0: # 전광판에 아무것도 안 띄워야하는 경우
-                ansarr[i] = -1
-                ans += bin(num[a[i]]).count('1')
-            elif ansarr[i-1] == -1: # 전광판에 아무것도 안 띄워야하는 경우
-                ansarr[i] = -1
-                ans += bin(num[a[i]]).count('1')
-            else:
-                
-
-
-
-
-
-        
-    
+    ast = ' '*(5-len(a)) + a
+    bst = ' '*(5-len(b)) + b
+    ans = 0
+    for i in range(5):
+        if ast[i] == ' ' and bst[i] == ' ': continue
+        elif ast[i] == ' ' : ans += bin( num[ int(bst[i]) ] ).count('1')
+        elif bst[i] == ' ' : ans += bin( num[ int(ast[i]) ] ).count('1')
+        else:ans += bin( num[ int(ast[i]) ] ^ num[ int(bst[i]) ]).count('1')
+    print(ans)
