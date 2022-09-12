aeb27f42ba688eec797375f9e2b48ba3402c4a9e
JHdesktop <thsehgml37@korea.ac.kr>
2022-09-12 22:39:15 +0900

    simulation

---
+++ basic/erastotenes.py
@@ -0,0 +1,12 @@
+n = int(input().rstrip())
+# p = [0,0]+[1]*~-n
+# for i in range(int(n**.5)+1):
+#     if p[i] : p[i*i::i] = [0]*(n//i-i+1)
+# p = [2] + [ i for i in range(3,n+1,2) if p[i]]
+# print(p)
+
+
+p = [0,0]+[1]*~-n
+for i in range(int(n**.5)+1):
+    if p[i] : p[i*i::i] = [0]*(n//i-i+1)
+p = [2] + [i for i in range(3,n+1,2) if p[i]]
--- softeer/x_안전운전을도와줄차세대지능형교통시스템.py
+++ softeer/x_안전운전을도와줄차세대지능형교통시스템.py
@@ -9,29 +9,34 @@ for y in range(n):
     for x in range(n) : g[y].append([*map(int,input().split())])
 
 ans = 1
-visit = [[0]*n for _ in range(n)]
-visit[0][0] = 1
-q = deque([(0,0,0)])
+visit = [ [0]*n for _ in range(n) ]
+
+visit[0][0] |= (1<<0)
+q = deque([(0,0,0,0)])
 dir = [(-1,0),(0,1),(1,0),(0,-1)] # 상0 우1 하2 좌3
 shinho = [(0,1,2), (3,0,1),(0,2,3),(1,2,3),   (0,1),(0,3),(2,3),(1,2),   (1,2),(0,1),(0,3),(2,3) ]
 #.           1.      2.      3.      4.        5.   6.    7.     8.        9.    10.  11.    12
 
 maxi = 0
+cango = [0]*4
+cango[0] |= (1<<1);cango[0] |= (1<<5);cango[0] |= (1<<9)
+cango[1] |= (1<<0);cango[1] |= (1<<4);cango[1] |= (1<<8)
+cango[2] |= (1<<3);cango[2] |= (1<<7);cango[2] |= (1<<11)
+cango[3] |= (1<<2);cango[3] |= (1<<6);cango[0] |= (1<<10)
 while q:
-    y,x,nowd = q.popleft()
-    lenn = len(g[y][x])
-    for tt,dd in enumerate(g[y][x]):
-        if nowd == 0 and (dd!=2 and dd!=6 and dd!=10):continue
-        if nowd == 1 and (dd!=1 and dd!=5 and dd!=9):continue
-        if nowd == 2 and (dd!=4 and dd!=8 and dd!=12):continue
-        if nowd == 3 and (dd!=3 and dd!=7 and dd!=11):continue
-        if tt != (visit[y][x]-1)%(lenn+1): continue
-        for d in shinho[dd-1]:
+    if ans == n*n:  break
+    y,x,nowd,time = q.popleft()
+    dd =  g[y][x][time%4] - 1
+
+    if (1<<dd) & cango[nowd] :
+        for d in shinho[dd]: 
             ny,nx = y+dir[d][0], x+dir[d][1]
             if ny<0 or nx<0 or ny>=n or nx>=n: continue
-            if visit[ny][nx]>0: continue
-            visit[ny][nx] = visit[y][x]+1
-            if visit[y][x] > t: continue
-            ans += 1
-            q.append((ny,nx, d))
+            if time+1<=t:
+                if visit[ny][nx] & (1<<d): continue
+                if visit[ny][nx] == 0 : ans+=1
+                visit[ny][nx] |= (1<<d)
+                
+                q.append((ny,nx, d, time+1))
+
 print(ans)
