071793f338c82aa2005222dea43dc557ac990e8a
doheeMac <thsehgml37@korea.ac.kr>
2022-09-21 16:13:39 +0900

    node and parent node

--- programmers/x_k호텔방배정___다시풀어보기.py
+++ programmers/x_k호텔방배정___다시풀어보기.py
@@ -2,11 +2,11 @@ import sys; sys.setrecursionlimit(10000)
 from collections import defaultdict
 
 def findEmpty(num, parent):
-    if parent[num] == 0:
+    if parent[num] == 0: # 빈방이므로 다음에 이 방 요청시에 그 다음 빈방 번호 업데이트해둠
         parent[num] = num+1
         return num
-    nextt = findEmpty(parent[num], parent)
-    parent[num] = nextt+1
+    nextt = findEmpty(parent[num], parent) # 해당방이 이미 찼으므로 부모노드 탐색
+    parent[num] = nextt+1 # 부모노드 갱신
     return nextt
 
 def solution(k, rn):
