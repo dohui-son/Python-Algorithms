ebe3cbb43e1abe8d5d26c84c3e7ab630d717d2d4
doheeMac <thsehgml37@korea.ac.kr>
2022-09-27 21:27:43 +0900

    stack

---
+++ baekjoon/b1918_후위표기식.py
@@ -0,0 +1,26 @@
+from collections import defaultdict,deque
+global st, orda,ordz
+st = deque( input() )
+n = len(st)
+stk = deque()
+orda, ordz = ord('A'), ord('Z')
+ans = ''
+
+for s in st:
+    if orda <= ord(s) <= ordz: ans += s
+    else:
+        if s == '(': stk.append(s)
+        elif s == '*' or s == '/' :
+            while stk and ( stk[-1] == '*' or stk[-1] == '/' ) : ans += stk.pop()
+            stk.append( s )
+        elif s == '+' or s == '-':
+            while stk and stk[-1] != '(': ans += stk.pop()
+            stk.append(s)
+        elif s == ')':
+            while stk and stk[-1] != '(': ans += stk.pop()
+            stk.pop()
+while stk: 
+    if stk[0] == '(' or stk[0] == ')': stk.pop(); continue
+    ans += stk.pop()
+
+print(ans)  
