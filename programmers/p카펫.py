2533c53e5b933378aa781b7f6b646f5c68ecc618
doheeMac <thsehgml37@korea.ac.kr>
2022-10-03 19:03:54 +0900

    Search

--- programmers/p카펫.py
+++ programmers/p카펫.py
@@ -1,10 +1,5 @@
-def solution(monster, S1, S2, S3):
-    die, suc, maxi = 0, 0, max(monster)
-      
-    for i in range(1, S1 + 1):
-        for  j in range(1, S2 + 1):
-            for k in range(1, S3 +1):
-                if i + j + k > maxi: suc += 1
-                elif (i + j + k + 1) in monster: die += 1
-                else: suc += 1
-    return int(suc / (die + suc) * 1000)
+def solution(brown, red):
+    for a in range(1, red + 1):
+        if red / a == red // a:
+            if a * 2 + (red // a) * 2 + 4 == brown: 
+                return [(red // a) + 2, a + 2] 
