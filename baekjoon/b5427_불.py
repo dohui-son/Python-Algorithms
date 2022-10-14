ad340ad26cbb65a1f0439dfc93c36a2e1f2877a3
doheeMac <thsehgml37@korea.ac.kr>
2022-10-14 13:23:12 +0900

    bfs and floyd

---
+++ baekjoon/b5427_불.py
@@ -0,0 +1,38 @@
+from collections import defaultdict, deque
+dir = [(0,1),(1,0),(-1,0),(0,-1)]
+def bfs(gg, r, c):
+    time = 0
+    q = deque()
+    fq = deque()
+    for y in range(r):
+        for x in range(c):
+            if gg[y][x] == '@': q.append((0,y,x))
+            elif gg[y][x] == '*': fq.append((0,y,x))
+    while q:        
+        time += 1
+        while fq and time > fq[0][0] :
+            ft, y,x = fq.popleft()
+            for d in dir:
+                ny, nx = y+d[0], x+d[1]
+                if ny<0 or nx<0 or ny>=r or nx>=c: continue
+                if gg[ny][nx] == '#'  or gg[ny][nx] == '*': continue
+                gg[ny][nx] = '*'
+                fq.appendleft((ft+1, ny,nx))
+        while q and time > q[0][0]:
+            cnt, y, x = q.popleft()
+            for d in dir:
+                ny, nx = y+d[0], x+d[1]
+                if ny<0 or nx<0 or ny>=r or nx>= c: return cnt+1
+                if gg[ny][nx] == '#' or gg[ny][nx] == '@' or gg[ny][nx] == '*': continue
+                if gg[ny][nx] == '.': 
+                    gg[ny][nx] = '@'
+                    q.append((cnt+1, ny, nx))
+    return -1
+
+t = int(input())
+for tt in range(t):
+    w, h = map(int ,input().split())
+    g = [ list(input()) for _ in range(h)]
+    res = bfs(g, h, w)
+    if res == -1: print('IMPOSSIBLE')
+    else: print(res)
